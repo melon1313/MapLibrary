6e0303590d523c754fff6d18fcae6ee80dd4559b
melon1313 <10212130@gm.nttu.edu.tw>
2022-05-17 04:19:43 +0800

    [Refactor] add new solution and add test case for new solution

--- MapLibrary.Tests/MapTests.cs
+++ MapLibrary.Tests/MapTests.cs
@@ -25,6 +25,7 @@ namespace MapLibrary.Tests
         [TestCase(67.234, "#f10000")]
         [TestCase(100.234, "#a80000")]
         [TestCase(64.491660, "#ff4242")]
+        [Category("Solution01")]
         public void GetHeatMapColor(double heatMapIndex, string colorCode)
         {
             var rtnVal = ColorTranslator.FromHtml(colorCode);
@@ -33,6 +34,35 @@ namespace MapLibrary.Tests
             
             var result = color.ToArgb();
             var expected = Color.FromArgb(Map.Alpha, rtnVal.R, rtnVal.G, rtnVal.B).ToArgb();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        
+        [TestCase(-5, "#b8de34")]
+        [TestCase(5.3, "#b8de34")]
+        [TestCase(7.234, "#bdff49")]
+        [TestCase(12.898332, "#bdff49")]
+        [TestCase(13.234, "#fff32c")]
+        [TestCase(23.234, "#ffff99")]
+        [TestCase(28.234, "#fff32c")]
+        [TestCase(37.234, "#eec713")]
+        [TestCase(43.234, "#ffb669")]
+        [TestCase(47.234, "#ff921e")]
+        [TestCase(57.234, "#ff6000")]
+        [TestCase(63.234, "#ff4242")]
+        [TestCase(67.234, "#f10000")]
+        [TestCase(100.234, "#a80000")]
+        [TestCase(64.491660, "#ff4242")]
+        [Category("Solution02")]
+        public void GetHeatMapColor_Solution02(double heatMapIndex, string colorCode)
+        {
+            var rtnVal = ColorTranslator.FromHtml(colorCode);
+            var color = Map_Solution02.GetHeatMapColorBy(heatMapIndex);
+
+
+            var result = color.ToArgb();
+            var expected = Color.FromArgb(Map_Solution02.Alpha, rtnVal.R, rtnVal.G, rtnVal.B).ToArgb();
 
             Assert.AreEqual(expected, result);
         }
---
+++ MapLibrary/Map_Solution02.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Drawing;
+
+namespace MapLibrary
+{
+    public class Map_Solution02
+    {
+        public static int Alpha => 70;
+
+        public static Color GetHeatMapColorBy(double heatMapIndex = 0)
+        {
+            var rtnVal = GetColorFromHtmlBy(heatMapIndex);
+
+            return Color.FromArgb(Alpha, rtnVal.R, rtnVal.G, rtnVal.B);
+        }
+
+        private static Color GetColorFromHtmlBy(double heatMapIndex)
+        {
+            foreach (var item in ColorLookUp())
+            {
+                var someClass = item as SomeClass;
+
+                var IsInRange = heatMapIndex.IsLessThanOrEqualToMaximum(someClass.HeatMapIndex);
+
+                if (IsInRange)
+                {
+                    return someClass.Color;
+                }
+            }
+
+            return Color.Gray;
+        }
+
+        private static IEnumerable ColorLookUp()
+        {
+            yield return new SomeClass { HeatMapIndex = 6.4491660, Color = ColorTranslator.FromHtml("#b8de34") };
+            yield return new SomeClass { HeatMapIndex = 12.898332, Color = ColorTranslator.FromHtml("#bdff49") };
+            yield return new SomeClass { HeatMapIndex = 19.347498, Color = ColorTranslator.FromHtml("#fff32c") };
+            yield return new SomeClass { HeatMapIndex = 25.796664, Color = ColorTranslator.FromHtml("#ffff99") };
+            yield return new SomeClass { HeatMapIndex = 32.245830, Color = ColorTranslator.FromHtml("#fff32c") };
+            yield return new SomeClass { HeatMapIndex = 38.694996, Color = ColorTranslator.FromHtml("#eec713") };
+            yield return new SomeClass { HeatMapIndex = 45.144162, Color = ColorTranslator.FromHtml("#ffb669") };
+            yield return new SomeClass { HeatMapIndex = 51.593328, Color = ColorTranslator.FromHtml("#ff921e") };
+            yield return new SomeClass { HeatMapIndex = 58.042494, Color = ColorTranslator.FromHtml("#ff6000") };
+            yield return new SomeClass { HeatMapIndex = 64.491660, Color = ColorTranslator.FromHtml("#ff4242") };
+            yield return new SomeClass { HeatMapIndex = 70.940826, Color = ColorTranslator.FromHtml("#f10000") };
+            yield return new SomeClass { HeatMapIndex = double.MaxValue, Color = ColorTranslator.FromHtml("#a80000") };
+        }
+
+        public class SomeClass
+        {
+            public double HeatMapIndex { get; set; }
+            public Color Color { get; set; }
+        }
+
+    }
+}
