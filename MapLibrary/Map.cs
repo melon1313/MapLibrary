95c43de00660b9f4b4fbac3b6aea646b967a7f0b
melon1313 <10212130@gm.nttu.edu.tw>
2022-05-17 00:49:11 +0800

    [Refactor] 語意化調整、抽出方法以凸顯意圖

--- MapLibrary/Map.cs
+++ MapLibrary/Map.cs
@@ -4,62 +4,93 @@ namespace MapLibrary
 {
 	public class Map
 	{
-		private static Color GetHeatMapColor(double heatMapIndex = 0)
-		{
-			Color rtnVal = Color.Gray;
-			if (heatMapIndex <= 6.449166)
-			{
-				rtnVal = ColorTranslator.FromHtml("#b8de34");
-			}
-			else if (heatMapIndex > 6.449166 && heatMapIndex <= 12.898332)
-			{
-				rtnVal = ColorTranslator.FromHtml("#bdff49");
-			}
-			else if (heatMapIndex > 12.898332 && heatMapIndex <= 19.347498)
-			{
-				rtnVal = ColorTranslator.FromHtml("#fff32c");
-			}
-			else if (heatMapIndex > 19.347498 && heatMapIndex <= 25.796664)
-			{
-				rtnVal = ColorTranslator.FromHtml("#ffff99");
-			}
-			else if (heatMapIndex > 25.796664 && heatMapIndex <= 32.245830)
-			{
-				rtnVal = ColorTranslator.FromHtml("#fff32c");
-			}
-			else if (heatMapIndex > 32.245830 && heatMapIndex <= 38.694996)
-			{
-				rtnVal = ColorTranslator.FromHtml("#eec713");
-			}
-			else if (heatMapIndex > 38.694996 && heatMapIndex <= 45.144162)
-			{
-				rtnVal = ColorTranslator.FromHtml("#ffb669");
-			}
-			else if (heatMapIndex > 45.144162 && heatMapIndex <= 51.593328)
-			{
-				rtnVal = ColorTranslator.FromHtml("#ff921e");
-			}
-			else if (heatMapIndex > 51.593328 && heatMapIndex <= 58.042494)
-			{
-				rtnVal = ColorTranslator.FromHtml("#ff6000");
-			}
-			else if (heatMapIndex > 58.042494 && heatMapIndex <= 64.491660)
-			{
-				rtnVal = ColorTranslator.FromHtml("#ff4242");
-			}
-			else if (heatMapIndex > 64.491660 && heatMapIndex <= 70.940826)
-			{
-				rtnVal = ColorTranslator.FromHtml("#f10000");
-			}
-			else if (heatMapIndex > 70.940826)
-			{
-				rtnVal = ColorTranslator.FromHtml("#a80000");
-			}
-			else
-			{
-				rtnVal = ColorTranslator.FromHtml("#b8de34");
-			}
+		public static int Alpha => 70;
 
-			return Color.FromArgb(70, rtnVal.R, rtnVal.G, rtnVal.B);
-		}
-	}
+		public static Color GetHeatMapColorBy(double heatMapIndex = 0)
+        {
+            Color rtnVal = Color.Gray;
+
+            rtnVal = GetColorFromHtmlBy(heatMapIndex);
+
+            return Color.FromArgb(Alpha, rtnVal.R, rtnVal.G, rtnVal.B);
+        }
+
+        private static Color GetColorFromHtmlBy(double heatMapIndex)
+        {
+            Color rtnVal;
+            if (heatMapIndex.IsLessThan(6.449166))
+            {
+                rtnVal = ColorTranslator.FromHtml("#b8de34");
+            }
+            else if (heatMapIndex.IsWithinInCludeMaximum(6.449166, 12.898332))
+            {
+                rtnVal = ColorTranslator.FromHtml("#bdff49");
+            }
+            else if (heatMapIndex.IsWithinInCludeMaximum(12.898332, 19.347498))
+            {
+                rtnVal = ColorTranslator.FromHtml("#fff32c");
+            }
+            else if (heatMapIndex.IsWithinInCludeMaximum(19.347498, 25.796664))
+            {
+                rtnVal = ColorTranslator.FromHtml("#ffff99");
+            }
+            else if (heatMapIndex.IsWithinInCludeMaximum(25.796664, 32.245830))
+            {
+                rtnVal = ColorTranslator.FromHtml("#fff32c");
+            }
+            else if (heatMapIndex.IsWithinInCludeMaximum(32.245830, 38.694996))
+            {
+                rtnVal = ColorTranslator.FromHtml("#eec713");
+            }
+            else if (heatMapIndex.IsWithinInCludeMaximum(38.694996, 45.144162))
+            {
+                rtnVal = ColorTranslator.FromHtml("#ffb669");
+            }
+            else if (heatMapIndex.IsWithinInCludeMaximum(45.144162, 51.593328))
+            {
+                rtnVal = ColorTranslator.FromHtml("#ff921e");
+            }
+            else if (heatMapIndex.IsWithinInCludeMaximum(51.593328, 58.042494))
+            {
+                rtnVal = ColorTranslator.FromHtml("#ff6000");
+            }
+            else if (heatMapIndex.IsWithinInCludeMaximum(58.042494, 64.491660))
+            {
+                rtnVal = ColorTranslator.FromHtml("#ff4242");
+            }
+            else if (heatMapIndex.IsWithinInCludeMaximum(64.491660, 70.940826))
+            {
+                rtnVal = ColorTranslator.FromHtml("#f10000");
+            }
+            else if (heatMapIndex.IsGreaterThan(70.940826))
+            {
+                rtnVal = ColorTranslator.FromHtml("#a80000");
+            }
+            else
+            {
+                rtnVal = ColorTranslator.FromHtml("#b8de34");
+            }
+
+            return rtnVal;
+        }
+
+    }
+
+	public static class CommonHelper
+    {
+        public static bool IsWithinInCludeMaximum(this double number, double minimum, double maximum)
+        {
+            return number > minimum && number <= maximum;
+        }
+
+        public static bool IsGreaterThan(this double number, double otherNumber)
+        {
+			return number > otherNumber;
+        }
+
+        public static bool IsLessThan(this double number, double otherNumber)
+        {
+            return number < otherNumber;
+        }
+    }
+}
